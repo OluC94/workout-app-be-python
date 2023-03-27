9f9a2f99f05cc3bff9180d19d3d1b033f6dfe8c1
OluC94 <coker.olu94@gmail.com>
2023-03-27 15:53:47 +0100

    initialise POST exercises/ endpoint

--- WorkoutAPI/ExercisesApp/views.py
+++ WorkoutAPI/ExercisesApp/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from WorkoutAPI.models import Exercises
 from WorkoutAPI.serializers import ExerciseSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 def exercise_list(request):
-    exercises = Exercises.objects.all() # get exercises
-    serializer = ExerciseSerializer(exercises, many=True)    # many=True to serialize whole list
-    return JsonResponse({"exercises": serializer.data}, safe=False)
+    if request.method == 'GET':
+        exercises = Exercises.objects.all() # get exercises
+        serializer = ExerciseSerializer(exercises, many=True)    # many=True to serialize whole list
+        return JsonResponse({'exercises': serializer.data})
+    
+    elif request.method == 'POST':
+        serializer = ExerciseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
