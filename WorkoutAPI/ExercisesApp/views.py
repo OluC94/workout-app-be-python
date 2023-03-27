07d48cb9106643bb4acb6b1880965ceac79e4c7a
OluC94 <coker.olu94@gmail.com>
2023-03-27 15:32:31 +0100

    setup serialization for exercise data

--- WorkoutAPI/ExercisesApp/views.py
+++ WorkoutAPI/ExercisesApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from WorkoutAPI.models import Exercises
+from WorkoutAPI.serializers import ExerciseSerializer
 
 # Create your views here.
+def exercise_list(request):
+    exercises = Exercises.objects.all() # get exercises
+    serializer = ExerciseSerializer(exercises, many=True)    # many=True to serialize whole list
+    return JsonResponse({"exercises": serializer.data}, safe=False)
