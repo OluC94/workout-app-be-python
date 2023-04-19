9ac1461af596bf812a69c03f293e05599fdbb397
OluC94 <coker.olu94@gmail.com>
2023-04-19 15:24:10 +0100

    fix Day serializer

--- WorkoutAPI/WorkoutAPI/serializers.py
+++ WorkoutAPI/WorkoutAPI/serializers.py
@@ -8,9 +8,18 @@ class ExerciseSerializer(serializers.ModelSerializer):
 
 class DaySerializer(serializers.ModelSerializer):
     
-    DayExercises = ExerciseSerializer(source='Exercises', read_only=True, many=True)
+    DayExercises = ExerciseSerializer(many=True)
 
     class Meta:
         model = Day
         fields = ['DayId', 'DayName', 'DayExercises']
         depth = 1
+
+    def create(self, validated_data):
+        exercise_data = validated_data.pop('DayExercises')
+        print(exercise_data)
+        day = Day.objects.create(**validated_data)
+        for data in exercise_data:
+            exercise = Exercises.objects.get_or_create(**data)
+            day.DayExercises.add(exercise)
+        return day
