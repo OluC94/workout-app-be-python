bf2497ba301cf752fd2f4d58e01b034e56787404
OluC94 <coker.olu94@gmail.com>
2023-05-11 14:46:52 +0100

    finish update function for Day serializer

--- WorkoutAPI/WorkoutAPI/serializers.py
+++ WorkoutAPI/WorkoutAPI/serializers.py
@@ -15,6 +15,10 @@ class DaySerializer(serializers.ModelSerializer):
         fields = ['DayId', 'DayName', 'DayExercises']
         depth = 1
         extra_kwargs = {'DayExercises': {'required': False}}
+    
+    def get_exercises(self, validated_data):
+        exercise_data = validated_data.pop('DayExercises')
+
 
     def create(self, validated_data):
         exercise_data = validated_data.pop('DayExercises')
@@ -24,5 +28,23 @@ class DaySerializer(serializers.ModelSerializer):
             day.DayExercises.add(exercise)
         return day
     
-    # def update()
+    def update(self, instance, validated_data):
+        exercise_data = validated_data.pop('DayExercises')
+        exercises = (instance.DayExercises).all()
+        exercises = list(exercises)
+        instance.DayName = validated_data.get('DayName', instance.DayName)
+        instance.save()
+
+        for _exercise in exercise_data:
+            exercise = exercises.pop(0)
+            exercise['ExerciseName'] = _exercise.get('ExerciseName', exercise['ExerciseName'])
+            exercise['Muscle'] = _exercise.get('Muscle', exercise['Muscle'])
+            exercise['Equipment'] = _exercise.get('Equipment', exercise['Equipment'])
+            exercise['Instructions'] = _exercise.get('Instructions', exercise['Instructions'])
+            exercise.save()
+        return instance
         
+        
+        
+            
+                
